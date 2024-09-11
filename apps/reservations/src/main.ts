53838d8e112462742b0366d74b299863e7f28c5d
do-not-do-that <rl7968@naver.com>
2024-09-11 12:39:59 +0900

    feat : 각각의 마이크로서비스 tcp 통신 연결

--- apps/reservations/src/main.ts
+++ apps/reservations/src/main.ts
@@ -4,9 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
+import { Transport } from '@nestjs/microservices';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationsModule);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+  });
+  app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -7,9 +7,10 @@ import {
   ReservationDocument,
   ReservationSchema,
 } from './models/reservation.schema';
-import { LoggerModule } from '@app/common';
-import { ConfigModule } from '@nestjs/config';
+import { LoggerModule, AUTH_SERVICE } from '@app/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
@@ -26,8 +27,23 @@ import * as Joi from 'joi';
       validationSchema: Joi.object({
         MONGODB_URI: Joi.string().required(),
         PORT: Joi.number().required(),
+        AUTH_HOST: Joi.string().required(),
+        AUTH_PORT: Joi.number().required(),
       }),
     }),
+    ClientsModule.registerAsync([
+      {
+        name: AUTH_SERVICE,
+        useFactory: (configSerivce: ConfigService) => ({
+          transport: Transport.TCP,
+          options: {
+            host: configSerivce.get<string>('AUTH_HOST'),
+            port: configSerivce.get<number>('AUTH_PORT'),
+          },
+        }),
+        inject: [ConfigService],
+      },
+    ]),
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationsRepository],
