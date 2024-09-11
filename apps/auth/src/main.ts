9f8684b665e040541c2ea43a8502970a1da5405b
do-not-do-that <rl7968@naver.com>
2024-09-11 12:21:44 +0900

    feat : 공통 인증가드 생성

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -4,9 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+  });
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,6 +19,7 @@ async function bootstrap() {
   );
   app.useLogger(app.get(Logger));
   const configService = app.get(ConfigService);
+  await app.startAllMicroservices();
   await app.listen(configService.get<number>('PORT'));
 }
 bootstrap();
