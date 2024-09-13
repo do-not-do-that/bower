8d84e96f8cc11b1b6c54c55c9ab368bfea53f517
do-not-do-that <rl7968@naver.com>
2024-09-13 17:21:21 +0900

    feat : 결제 생성 서비스 구현

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -11,4 +11,24 @@ export class PaymentsService {
     },
   );
   constructor(private readonly configSerivce: ConfigService) {}
+
+  async createCharge(
+    card: Stripe.PaymentMethodCreateParams.Card,
+    amount: number,
+  ) {
+    const paymentMethod = await this.stripe.paymentMethods.create({
+      type: 'card',
+      card,
+    });
+
+    const paymentIntent = await this.stripe.paymentIntents.create({
+      payment_method: paymentMethod.id,
+      amount: amount * 100,
+      confirm: true,
+      payment_method_types: ['card'],
+      currency: 'usd',
+    });
+
+    return paymentIntent;
+  }
 }
